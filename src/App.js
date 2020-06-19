62933220bba9bc17a5397d60d1e71de6cb647f2c
Alex Barbosa <alexfelipebarbosa@yahoo.com.br>
2020-06-18 21:24:14 -0300

    Aula 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,51 @@
 import React, { Component } from 'react';
-import ProjetoBase from './components/ProjetoBase/ProjetoBase';
 
 export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      allCountries: [],
+    };
+  }
+
+  async componentDidMount() {
+    const res = await fetch('https://restcountries.eu/rest/v2/all');
+    const json = await res.json();
+
+    const allCountries = json.map(
+      ({ name, numericCode, flag, population, area }) => {
+        return {
+          id: numericCode,
+          name,
+          flag,
+          population,
+          area,
+        };
+      }
+    );
+
+    this.setState({
+      allCountries: allCountries,
+    });
+  }
+
   render() {
-    return <ProjetoBase />;
+    const { allCountries } = this.state;
+
+    console.log(allCountries);
+    return (
+      <div className="container">
+        <h5>
+          Desafio realizado no Modulo de React do BootCamp FullStack do IGTI
+        </h5>
+        <h6>
+          -- Criação de um app para listar países a partir da API
+          https://restcountries.eu/rest/v2/all
+        </h6>
+        <h6>-- Input para filtrar os paises</h6>
+        <h1>React Countries </h1>
+      </div>
+    );
   }
 }
