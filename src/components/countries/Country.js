9518993db310efcc374f764d95518eec11eb5fa9
Alex Barbosa <alexfelipebarbosa@yahoo.com.br>
2020-06-22 15:16:03 -0300

    Ajustes no estilo

--- src/components/countries/Country.js
+++ src/components/countries/Country.js
@@ -4,7 +4,15 @@ import css from './countries.module.css';
 export default class Country extends Component {
   render() {
     const { country } = this.props;
+    const { name, flag } = country;
 
-    return <div className={`${css.country} ${css.border}`}>{country.name}</div>;
+    return (
+      <div className={`${css.country} ${css.border}`}>
+        <img className={css.flag} src={flag} alt={name} />
+        <span className={css.countryName}>
+          <strong>{name}</strong>
+        </span>
+      </div>
+    );
   }
 }
